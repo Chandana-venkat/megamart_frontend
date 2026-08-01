ad8362abb91e98f3f875a22ccd7481b4c4f63ae1
Edupalli Chandana <23kq5a0410ece@gmail.com>
2026-08-01 13:12:21 +0530

    final code  of login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,187 +1,6 @@
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import Nav from "../components/Nav";
-// import Footer from "../components/Footer";
-// import "../styles/Login.css";
-
-// function Login() {
-
-//   const navigate = useNavigate();
-
-//   const [data, setData] = useState({
-//     email: "",
-//     password: "",
-//   });
-
-//   const [errors, setErrors] = useState({});
-
-//   const handleChange = (e) => {
-
-//     const { name, value } = e.target;
-
-//     setData({
-//       ...data,
-//       [name]: value,
-//     });
-
-//     let error = "";
-
-//     if (name === "email") {
-
-//       if (value.trim() === "") {
-
-//         error = "Email is required";
-
-//       } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
-
-//         error = "Enter valid email";
-
-//       }
-
-//     }
-
-//     if (name === "password") {
-
-//       if (value.trim() === "") {
-
-//         error = "Password is required";
-
-//       } else if (value.length < 6) {
-
-//         error = "Password must be at least 6 characters";
-
-//       }
-
-//     }
-
-//     setErrors({
-//       ...errors,
-//       [name]: error,
-//     });
-
-//   };
-
-//   const login = async (e) => {
-
-//     e.preventDefault();
-
-//     let newErrors = {};
-
-//     if (data.email.trim() === "") {
-
-//       newErrors.email = "Email is required";
-
-//     }
-
-//     if (data.password.trim() === "") {
-
-//       newErrors.password = "Password is required";
-
-//     }
-
-//     setErrors(newErrors);
-
-//     if (Object.keys(newErrors).length > 0) return;
-
-//     try {
-
-//       const response = await fetch(
-//         `http://localhost:3001/users?email=${data.email}&password=${data.password}`
-//       );
-
-//       if (!response.ok) {
-
-//         throw new Error("Server Error");
-
-//       }
-
-//       const users = await response.json();
-
-//       if (users.length > 0) {
-
-//         const loggedUser = users[0];
-
-//         localStorage.setItem(
-//           "user",
-//           JSON.stringify(loggedUser)
-//         );
-
-//         alert("Login Successful");
-
-//         setData({
-//           email: "",
-//           password: "",
-//         });
-
-//         navigate("/");
-
-//       } else {
-
-//         alert("Invalid Email or Password");
-
-//       }
-
-//     } catch (error) {
-
-//       alert(error.message);
-
-//     }
-
-//   };
-
-//   return (
-//     <>
-//       <Nav />
-
-//       <div className="register-container">
-
-//         <h1>Login</h1>
-
-//         <form onSubmit={login}>
-
-//           <input
-//             type="email"
-//             name="email"
-//             placeholder="Enter Email"
-//             value={data.email}
-//             onChange={handleChange}
-//           />
-
-//           <p className="error">
-//             {errors.email}
-//           </p>
-
-//           <input
-//             type="password"
-//             name="password"
-//             placeholder="Enter Password"
-//             value={data.password}
-//             onChange={handleChange}
-//           />
-
-//           <p className="error">
-//             {errors.password}
-//           </p>
-
-//           <button type="submit">
-//             Login
-//           </button>
-
-//         </form>
-
-//       </div>
-
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default Login;
-
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import Footer from "../components/Footer";
 import "../styles/Login.css";
 import API from "../services/api";
 
@@ -192,25 +11,25 @@ function Login(){
 
 
   const [data,setData]=useState({
-
     email:"",
     password:""
-
   });
 
-const [errors,setErrors]=useState({});
 
- const handleChange=(e)=>{
+  const [errors,setErrors]=useState({});
+
+
+
+  const handleChange=(e)=>{
 
     const {name,value}=e.target;
 
 
     setData({
-
       ...data,
       [name]:value
-
     });
+
 
 
     let error="";
@@ -233,9 +52,11 @@ const [errors,setErrors]=useState({});
 
     }
 
-  if(name==="password"){
 
-      if(value===""){
+
+    if(name==="password"){
+
+      if(value.trim()===""){
 
         error="Password is required";
 
@@ -251,87 +72,136 @@ const [errors,setErrors]=useState({});
 
 
     setErrors({
-
       ...errors,
       [name]:error
-
     });
 
 
   };
-const handleLogin = async (e) => {
 
-  e.preventDefault();
 
-  let newErrors = {};
 
-  if (data.email.trim() === "") {
-    newErrors.email = "Email is required";
-  }
 
-  if (data.password.trim() === "") {
-    newErrors.password = "Password is required";
-  }
+  const handleLogin = async(e)=>{
 
-  setErrors(newErrors);
 
-  if (Object.keys(newErrors).length > 0) {
-    return;
-  }
+    e.preventDefault();
 
-  try {
 
-    // const response = await fetch(
-    //   `http://localhost:3001/users?email=${data.email}&password=${data.password}`
-    // );
 
-    // const users = await response.json();
-    const response = await API.get(
-  `/users?email=${data.email}&password=${data.password}`
-   );
+    let newErrors={};
 
-const users = response.data;
 
-    if(users.length > 0){
 
-        // localStorage.setItem(
-        //     "user",
-        //     JSON.stringify(users[0])
-        // );
+    if(data.email.trim()===""){
+
+      newErrors.email="Email is required";
+
+    }
+
+
+
+    if(data.password.trim()===""){
+
+      newErrors.password="Password is required";
+
+    }
+
+
+
+    setErrors(newErrors);
+
+
+
+    if(Object.keys(newErrors).length>0){
+
+      return;
+
+    }
+
+
+
+
+    try{
+
+
+      // Get all users from backend
+
+      const response = await API.get("/users");
+
+
+      const users = response.data;
+
+
+
+      console.log("Users:",users);
+
+
+
+      const loggedUser = users.find(
+
+        (user)=>
+
+          user.email === data.email.trim()
+          &&
+          user.password === data.password.trim()
+
+      );
+
+
+
+      if(loggedUser){
+
+
 
         localStorage.setItem(
-      "currentUser",
-         JSON.stringify(users[0])
-     );
+
+          "currentUser",
+
+          JSON.stringify(loggedUser)
+
+        );
+
+
+
         alert("Login Successful 🎉");
+
+
 
         navigate("/");
 
-    }
-    else{
+
+      }
+      else{
+
 
         alert("Invalid Email Or Password");
 
+
+      }
+
+
+
+    }
+    catch(error){
+
+
+      console.log(error);
+
+      alert("Server Error");
+
+
     }
 
 
-}
-catch(error){
+  };
 
-    console.log(error);
 
-    alert("Server Error");
-
-}
-
-};
 
 
   return(
 
-    <>
-
- <div className="login-container">
+    <div className="login-container">
 
 
       <h1>Login</h1>
@@ -343,23 +213,22 @@ catch(error){
 
         <input
 
-        type="email"
+          type="email"
 
-        name="email"
+          name="email"
 
-        placeholder="Enter Email"
+          placeholder="Enter Email"
 
-        value={data.email}
+          value={data.email}
 
-        onChange={handleChange}
+          onChange={handleChange}
 
         />
 
 
+
         <p className="error">
-
-        {errors.email}
-
+          {errors.email}
         </p>
 
 
@@ -367,31 +236,30 @@ catch(error){
 
         <input
 
-        type="password"
+          type="password"
 
-        name="password"
+          name="password"
 
-        placeholder="Enter Password"
+          placeholder="Enter Password"
 
-        value={data.password}
+          value={data.password}
 
-        onChange={handleChange}
+          onChange={handleChange}
 
         />
 
 
+
         <p className="error">
-
-        {errors.password}
-
+          {errors.password}
         </p>
 
 
 
 
-        <button>
+        <button type="submit">
 
-        Login
+          Login
 
         </button>
 
@@ -400,13 +268,8 @@ catch(error){
       </form>
 
 
+
     </div>
-
-
-   
-
-
-    </>
 
   );
 
