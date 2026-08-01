6b1a5c5c8e3d3fe8e824f0324f4122c3e848cf3d
Edupalli Chandana <23kq5a0410ece@gmail.com>
2026-08-01 12:34:22 +0530

    render url in api.js

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,16 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//     // baseURL: "http://localhost:3000"
+
+//     baseURL:" https://megamart-backend-foau.onrender.com/products"
+// });
+
+// export default API;
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://megamart-backend-foau.onrender.com"
 });
 
 export default API;
