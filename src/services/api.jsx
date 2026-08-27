4c5343d246aa34d514b5a531870c8c590e82a68e
Edupalli Chandana <23kq5a0410ece@gmail.com>
2026-08-28 00:44:52 +0530

    Connect frontend to Docker backend

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -7,11 +7,21 @@
 // });
 
 // export default API;
+//----------------------------------------
+// import axios from "axios";
+
+// const API = axios.create({
+//     // baseURL: "https://megamart-backend-foau.onrender.com"
+//     baseURL: "http://localhost:9090"
+// });
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-    // baseURL: "https://megamart-backend-foau.onrender.com"
-    baseURL: "http://localhost:9090"
+    baseURL: "https://pct-metadata-engine-pounds.trycloudflare.com"
 });
 
 export default API;
